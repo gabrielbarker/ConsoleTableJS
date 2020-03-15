ee43fc5e6c12cb925087f93232941694c8160fee
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-15 17:57:13 +0000

    ADD an API for the extension
    The Tablo class will be exposed as the main endpoint

---
+++ src/Tablo.ts
@@ -0,0 +1,43 @@
+import TableStyleBuilder from "./TableStyleBuilder";
+import { VerticalLineCharacter, HorizontalLineCharacter } from "./TableStyleBuilder";
+import ObjectValidator from "./ObjectValidator";
+import ColumnExtractor from "./ColumnExtractor";
+import Column from "./Column";
+import TablePrinter from "./TablePrinter";
+
+export default class Tablo {
+  private columns: Column[];
+  private styleBuilder: TableStyleBuilder = new TableStyleBuilder();
+  private validator: ObjectValidator = new ObjectValidator();
+
+  constructor(object: any) {
+    if (this.validator.isValid(object)) {
+      this.columns = new ColumnExtractor(object).getColumns();
+    } else throw "Invalid Object";
+  }
+
+  public print(): void {
+    const printer = new TablePrinter(this.columns, this.styleBuilder.build());
+    printer.print();
+  }
+
+  public withNumberOfSpacesAtStartOfColumns(spaces: number): Tablo {
+    this.styleBuilder = this.styleBuilder.withNumberOfSpacesAtStartOfColumns(spaces);
+    return this;
+  }
+
+  public withNumberOfSpacesAtEndOfColumns(spaces: number): Tablo {
+    this.styleBuilder = this.styleBuilder.withNumberOfSpacesAtEndOfColumns(spaces);
+    return this;
+  }
+
+  public withVerticalLineStyle(style: VerticalLineCharacter): Tablo {
+    this.styleBuilder = this.styleBuilder.withVerticalLineStyle(style);
+    return this;
+  }
+
+  public withHorizontalLineStyle(style: HorizontalLineCharacter): Tablo {
+    this.styleBuilder = this.styleBuilder.withHorizontalLineStyle(style);
+    return this;
+  }
+}
