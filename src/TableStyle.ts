e9b0d755f40cf3df4a751c9a119abf6554345743
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-15 17:43:48 +0000

    REMOVE the restrictions on line characters
    This will be handled by the builder instead

--- src/TableStyle.ts
+++ src/TableStyle.ts
@@ -1,15 +1,10 @@
 export default class TableStyle {
   public readonly spaceStart: string;
   public readonly spaceEnd: string;
-  public readonly verticalLine: ":" | "|" | " ";
-  public readonly horizontalLine: "-" | "=";
+  public readonly verticalLine: string;
+  public readonly horizontalLine: string;
 
-  constructor(
-    spaceStart: string,
-    spaceEnd: string,
-    verticalLine: ":" | "|" | " ",
-    horizontalLine: "-" | "="
-  ) {
+  constructor(spaceStart: string, spaceEnd: string, verticalLine: string, horizontalLine: string) {
     this.spaceStart = spaceStart;
     this.spaceEnd = spaceEnd;
     this.verticalLine = verticalLine;
