6bc18d659db87a9068f8aeae5a698925508cbad1
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-27 23:07:58 +0000

    MAKE all messages readonly

--- src/Messages.ts
+++ src/Messages.ts
@@ -1,8 +1,8 @@
 export default class Messages {
-  public static ONE_ARRAY_FIELD_PER_OBJECT: string =
+  public static readonly ONE_ARRAY_FIELD_PER_OBJECT: string =
     "Invalid Taybl Data: Only one field per object can be an array.";
-  public static ARRAY_FIELD_MUST_CONTAIN_OBJECTS: string =
+  public static readonly ARRAY_FIELD_MUST_CONTAIN_OBJECTS: string =
     "Invalid Taybl Data: Array fields must contain objects.";
-  public static ARRAY_FIELDS_MUST_HAVE_ONE_TYPE: string =
+  public static readonly ARRAY_FIELDS_MUST_HAVE_ONE_TYPE: string =
     "Invalid Taybl Data: Array fields must have consistent type.";
 }
