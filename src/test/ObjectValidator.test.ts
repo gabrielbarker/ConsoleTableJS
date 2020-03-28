a4d794c8344699bd8c6259f76295bc00f4128283
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-28 00:39:05 +0000

    ADD additional test cases for the object validator

--- src/test/ObjectValidator.test.ts
+++ src/test/ObjectValidator.test.ts
@@ -1,50 +1,82 @@
-import { assert } from "chai";
+import { assert, expect } from "chai";
 import "mocha";
 import ObjectValidator from "../ObjectValidator";
+import Messages from "../Messages";
 
-describe("ObjectValidator: isValid", () => {
+describe("ObjectValidator: isValid - with valid objects", () => {
   it("should return true for valid objects", () => {
-    const object = {
-      firstField: "first field value",
-      objectField: [{ secondField: "second field value1" }, { secondField: "second field value2" }]
-    };
     const validator = new ObjectValidator();
-    const valid = validator.isValid(object);
+    const valid = validator.isValid(VALID_OBJECT);
     assert(valid);
   });
 
   it("should return true for array of valid objects", () => {
-    const object = [
-      {
-        fileName: "file name1",
-        invalid: [
-          { type: "type name1", count: 6, "line numbers": "7, 18" },
-          { type: "type name2", count: 2, "line numbers": "17, 9" }
-        ]
-      },
-      {
-        fileName: "file name2",
-        invalid: [
-          { type: "type name3", count: 0, "line numbers": "28" },
-          { type: "type name4", count: 3, "line numbers": "1, 9, 12" }
-        ]
-      }
-    ];
     const validator = new ObjectValidator();
-    const valid = validator.isValid(object);
+    const valid = validator.isValid(ARRAY_OF_VALID_OBJECTS);
     assert(valid);
   });
+});
 
-  it("should return false for invalid valid objects", () => {
-    const object = {
-      fileName: "file name1",
-      invalid: [
-        { type: "type name1", count: 6, "line numbers": "7, 18" },
-        { type: "type name2", otherField: 2, "line numbers": "17, 9" }
-      ]
-    };
+describe("ObjectValidator: isValid, getMessage - with invalid objects", () => {
+  it("should return false for objects with different fields", () => {
     const validator = new ObjectValidator();
-    const valid = validator.isValid(object);
-    assert(!valid);
+    const valid = validator.isValid(IVALID_OBJECT_DIFFERENT_FIELDS);
+    assert(!valid, "Arrays contianing objects with different fields should be invalid");
+    expect(validator.getMessage()).to.equal(Messages.ARRAY_FIELDS_MUST_HAVE_ONE_TYPE);
+  });
+
+  it("should return false for objects with multiple array fields", () => {
+    const validator = new ObjectValidator();
+    const valid = validator.isValid(IVALID_OBJECT_MULTIPLE_ARRAY_FIELDS);
+    assert(!valid, "Objects with more than one array field should be invalid");
+    expect(validator.getMessage()).to.equal(Messages.ONE_ARRAY_FIELD_PER_OBJECT);
+  });
+
+  it("should return false for objects whose array fields contain primitives", () => {
+    const validator = new ObjectValidator();
+    const valid = validator.isValid(IVALID_OBJECT_ARRAY_WITH_PRIMITIVES);
+    assert(!valid, "Objects more than one array field should be invalid");
+    expect(validator.getMessage()).to.equal(Messages.ARRAY_FIELD_MUST_CONTAIN_OBJECTS);
   });
 });
+
+const VALID_OBJECT = {
+  firstField: "first field value",
+  objectField: [{ secondField: "second field value1" }, { secondField: "second field value2" }]
+};
+
+const ARRAY_OF_VALID_OBJECTS = [
+  {
+    firstField: "first field value1",
+    objectField: [{ secondField: "second field value1" }, { secondField: "second field value2" }]
+  },
+  {
+    firstField: "first field value2",
+    objectField: [{ secondField: "second field value3" }, { secondField: "second field value4" }]
+  }
+];
+
+const IVALID_OBJECT_DIFFERENT_FIELDS = {
+  fileName: "file name1",
+  invalid: [
+    { type: "type name1", count: 6, "line numbers": "7, 18" },
+    { type: "type name2", otherField: 2, "line numbers": "17, 9" }
+  ]
+};
+
+const IVALID_OBJECT_MULTIPLE_ARRAY_FIELDS = {
+  fileName: "file name1",
+  invalid: [
+    { type: "type name1", count: 6, "line numbers": "7, 18" },
+    { type: "type name2", otherField: 2, "line numbers": "17, 9" }
+  ],
+  valid: [
+    { type: "type name1", count: 6, "line numbers": "7, 18" },
+    { type: "type name2", otherField: 2, "line numbers": "17, 9" }
+  ]
+};
+
+const IVALID_OBJECT_ARRAY_WITH_PRIMITIVES = {
+  fileName: "file name1",
+  invalid: ["value1", "value2", "value3"]
+};
